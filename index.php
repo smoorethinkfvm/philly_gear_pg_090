2d1f68baa25dc25506ba1b7bc1faf6ff490f4a18
smoorethinkfvm <41012114+smoorethinkfvm@users.noreply.github.com>
2018-07-09 11:05:53 -0400

    added files

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <link rel="stylesheet" href="https://use.typekit.net/aqn1bga.css">
+        <link href="style.css" rel="stylesheet">
+        
+        <!-- Google Analytics -->
+        <script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		  ga('create', 'UA-32300410-1', 'auto');
+		  ga('send', 'pageview');
+		</script>
+        
+    </head>
+    <body>
+    
+        <?php $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>
+    
+        <section id="header">
+        	<div class="container">
+            	<div class="col-xs-12">
+            		<div class="logo">
+            		    <img src="images/logo.png" alt="Philadelphia Gear" />
+            		</div>
+            	</div>
+        	</div>
+        </section>
+        
+        <?php if (false !== strpos($url,'?submission=received' )) { ?>
+        
+            <section id="message">
+            	<div class="container">
+            		<div class="col-xs-12 col-md-10 col-md-offset-1 text-center">
+            			
+                        <!-- <h2>Thank you! Your submission has been received.</h2> -->		
+            			<h2>Thank you! Your request has been submitted.</h2>
+            						
+            		</div>
+            	</div>
+            </section>
+        
+        <?php } ?>
+        
+        <section id="marquee" class="vertical-align">
+        	<div class="backgrounds">
+            	<div class="marquee-bg"></div>
+            	<div class="marquee-gif"></div>
+        	</div>
+        	<div class="wrapper">
+            	<div class="content">
+            		<div class="container">
+            		    <div class="col-md-8">
+                			<h1>You wouldn't settle for part of a hotel room</h1>
+                			<h2>Why settle for part of a drivetrain service solution?</h2>
+            			</div>
+            			<div class="clearfix"></div>
+            		</div>
+            	</div>
+        	</div>
+        </section>
+        
+        <section id="marqueeText" class="vertical-align">
+        	<div class="wrapper">
+            	<div class="content">
+            		<div class="container">
+            		    <div class="col-md-8">
+                			<h1>You wouldn't settle for part of a hotel room</h1>
+                			<h2>Why settle for part of a drivetrain service solution?</h2>
+            			</div>
+            			<div class="clearfix"></div>
+            		</div>
+            	</div>
+        	</div>
+        </section>
+        
+        <section id="peace">
+        	<div class="row-eq-height">
+            	<div class="col-sm-6 left"></div>
+            	<div class="col-sm-6 right">
+            		<div class="col-xs-10 col-xs-offset-1 col-md-8 col-lg-7">
+            			<p>If you're still betting your drivetrain on service providers that can only repair individual components, you could be in for a lot of long-term headaches... and a lot of sleepless nights.</p>
+                		<p>Peace of mind comes from working with a single trusted source, a provider who can get you to what you need, when you need it, every time.</p>
+            		</div>
+            		<div class="clearfix"></div>
+            	</div>
+        	</div>
+        </section>
+        
+        <section id="restEasy">
+        	<div class="container">
+        		<div class="col-md-7">
+        			<h2>Rest easy with a complete solution from Philadelphia Gear</h2>
+        			<p>As part of Timken Power Systems, Philadelphia Gear has expanded services to include inspections, upgrades, and repairs to every critical drivetrain component. With Timken Power Systems, you gain access to eight proven providers for complete support, just one call away.</p>
+        		</div>
+        		<div class="col-md-5">
+        			<img src="images/door-sign-animation-20180220.gif">
+        		</div>
+        	</div>
+        </section>
+        
+        <section id="brands">
+        	<div class="container">
+        		<div class="col-xs-12 gears">
+        			<ul class="list-inline">
+        				<li><img src="images/gear-bearing.jpg" width="27" height="27"></li>
+        				<li>Bearing Repair</li>
+        				<li><img src="images/gear-electric.jpg" width="27" height="27"></li>
+        				<li>Electric</li>
+        				<li><img src="images/gear-gear.jpg" width="27" height="27"></li>
+        				<li>Gear</li>
+        			</ul>
+        		</div>
+        		<div class="text-center">
+            		<div class="col-xs-6 col-md-4 col-lg-3 brand">
+            			<img src="images/client-phillygear.jpg">
+            			<p><img src="images/gear-gear.jpg" width="27" height="27"></p>
+            		</div>
+            		<div class="col-xs-6 col-md-4 col-lg-3 brand">
+            			<img src="images/client-standard.jpg">
+            			<p><img src="images/gear-gear.jpg" width="27" height="27"> <img src="images/gear-bearing.jpg" width="27" height="27"></p>
+            		</div>
+            		<div class="col-xs-6 col-md-4 col-lg-3 brand">
+            			<img src="images/client-smith.jpg">
+            			<p><img src="images/gear-electric.jpg" width="27" height="27"> <img src="images/gear-gear.jpg" width="27" height="27"></p>
+            		</div>
+            		<div class="col-xs-6 col-md-4 col-lg-3 brand">
+            			<img src="images/client-wazee.jpg">
+            			<p><img src="images/gear-electric.jpg" width="27" height="27"> <img src="images/gear-gear.jpg" width="27" height="27"></p>
+            		</div>
+            		<div class="col-xs-6 col-md-4 col-lg-3 brand">
+            			<img src="images/client-hnelectric.jpg">
+            			<p><img src="images/gear-electric.jpg" width="27" height="27"> <img src="images/gear-gear.jpg" width="27" height="27"></p>
+            		</div>
+            		<div class="col-xs-6 col-md-4 col-lg-3 brand">
+            			<img src="images/client-hnwind.jpg">
+            			<p><img src="images/gear-electric.jpg" width="27" height="27"> <img src="images/gear-gear.jpg" width="27" height="27"></p>
+            		</div>
+            		<div class="col-xs-6 col-md-4 col-md-offset-1 col-lg-3 col-lg-offset-0 brand">
+            			<img src="images/client-schulz.jpg">
+            			<p><img src="images/gear-electric.jpg" width="27" height="27"> <img src="images/gear-gear.jpg" width="27" height="27"></p>
+            		</div>
+            		<div class="col-xs-6 col-md-4 col-md-offset-1 col-lg-3 col-lg-offset-0 brand">
+            			<img src="images/client-timken.jpg">
+            			<p><img src="images/gear-bearing.jpg" width="27" height="27"></p>
+            		</div>
+        		</div>
+        	</div>
+        </section>
+        
+        <section id="contact">
+        	<div class="container">
+        		<div class="col-md-8">
+        			<p>Gear products and services. Bearing repairs. Electric products and services. No matter what you need, Timken Power Systems means one point of contact for 24/7 on-site assistance from our network of ISO 9001-certified service experts.</p>
+        		</div>
+        	</div>
+        </section>
+        
+        <section id="services">
+        	<div class="container">
+        		<div class="col-xs-12 text-center">
+        		    <h3>Services available throughout the timken power systems network</h3>
+        		</div>
+        		<div class="clearfix"></div>
+        		<div class="col-sm-4">
+        			<p class="icon electric">Electric Motor Services</p>
+        			<ul>
+        				<li>AC/DC motor and generator repair and rewinds</li>
+        				<li>VFD and electronic controls repair</li>
+        				<li>Motor redesigns and custom controls</li>
+        				<li>New component sales services</li>
+        				<li>Critical spares storage program</li>
+        			</ul>
+        		</div>
+        		<div class="col-sm-4">
+        			<p class="icon gear">Gear Repair Services</p>
+        			<ul>
+        				<li>New and replacement gear drives</li>
+        				<li>In-place overhauls</li>
+        				<li>Reverse engineering and uprating</li>
+        				<li>Inspections, repairs, and upgrades</li>
+        				<li>Parts manufactured to OEM specs</li>
+        			</ul>
+        		</div>
+        		<div class="col-sm-4">
+        			<p class="icon bearing">Bearing Repair Services</p>
+        			<ul>
+        				<li>Repair bearings 254 mm to 6069 mm</li>
+        				<li>On-site inspection services</li>
+        				<li>Repair transportation and logistics</li>
+        				<li>Any bearing type, any manufacturer</li>
+        			</ul>
+        		</div>
+        		<div class="clearfix"></div>
+        	</div>
+        </section>
+        
+        <section id="form">
+        	<div class="container">
+        		<div class="col-xs-12 col-md-10 col-md-offset-1 text-center">
+        			<h2>What could a full suite of services mean for your operations?</h2>
+        			<p>In the new white paper <strong><em>Diagnostic and Preventative Maintenance Procedures for Industrial Powertrains: A Holistic Approach</em></strong>, you'll learn what a change in service mindset can and has accomplished for organizations seeking a more reliable way to maintain their drivetrains day-in and day-out.</p>
+        			<p>Fill in your information below to request your copy of this important white paper today.</p>
+        		</div>
+        		<div class="col-xs-8 col-xs-offset-2 col-sm-3 col-sm-offset-0">
+        			<img src="images/whitepaper.jpg">
+        		</div>
+        		<div class="col-xs-12 col-sm-6">
+        			<link href="https://static-cdn.e2ma.net/signups/css/signup-refresh.med.css" rel="stylesheet" type="text/css"><script type="text/javascript" src="https://signup.e2ma.net/tts_signup/1868315/d9e55ee704403343c3cc582e2bfd0440/1779800/"></script><div id="load_check" class="signup_form_message" >This form needs Javascript to display, which your browser doesn't support. <a href="https://signup.e2ma.net/signup/1868315/1779800/"> Sign up here</a> instead </div><script type="text/javascript">signupFormObj.drawForm();</script>
+        		</div>
+        	</div>
+        </section>
+        
+        <section id="footer">
+        	<div class="container">
+        		<div class="col-xs-12 text-center">
+        			<ul class="list-inline">
+        				<li>
+        				    <a href="https://www.timkenpowersystems.com/">
+        				    	Home
+        				    </a>
+        				</li>
+        				<li>
+        				    <a href="https://timkenpowersystems.com/about.html">
+        				    	About
+        				    </a>
+        				</li>
+        				<li>
+        				    <a href="https://timkenpowersystems.com/products-services.html">
+        				    	Products & Services
+        				    </a>
+        				</li>
+        				<li>
+        				    <a href="http://news.timken.com/">
+        				    	News
+        				    </a>
+        				</li>
+        				<li>
+        				    <a href="https://www.timkenpowersystems.com/events/">
+        				    	Events
+        				    </a>
+        				</li>
+        			</ul>
+        			<p>&copy; 2002 - <?php echo date("Y"); ?> Philadelphia Gear, Power Systems by Timken. All rights reserved.</p> 
+        		</div>
+        	</div>
+        </section>
+    
+        <!-- Bootstrap -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        
+        <!-- Crazy Egg -->
+        <script type="text/javascript" src="//script.crazyegg.com/pages/scripts/0044/3836.js" async="async"></script>
+        
+        <!-- Global site tag (gtag.js) - AdWords: 877342692 --> 
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-877342692"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-877342692'); </script>
+        
+        <!-- Event Tracking -->
+        <script>
+        	$("#id_member_field_first_name").on("click", function() {
+        	    ga('send', 'event', { eventCategory: 'Partial Solution - 2018', eventAction: 'Contact', eventLabel: 'Form Initiation'});
+        	});
+        	$("#e2ma_signup_submit_button").on("click", function() {
+        	    ga('send', 'event', { eventCategory: 'Partial Solution - 2018', eventAction: 'Contact', eventLabel: 'Form Submission'});
+        	});
+        </script>
+        
+    </body>
+</html>
---
+++ philly_gear_PG_090/README.md
@@ -0,0 +1,1 @@
+# philly_gear_PG_090
